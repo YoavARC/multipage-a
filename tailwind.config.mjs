be9b0cedb6e22fd3e8f9ae3be7bd45c3c24d2735
Kevin192212 <kppardo0401@gmail.com>
2024-10-31 09:59:53 -0500

    cuarta version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
+import _default from 'astro/runtime/client/idle.prebuilt.js';
+
+import _defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+				'myFont':['"myFont"', ..._defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 }
+
+
+//yoav alejandro ronquillo cruz
+//22 years
+//22/05/2002
+//mexicano
